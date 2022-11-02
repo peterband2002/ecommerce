7e7b42887b5e3a6ef351aec43e9591fd18d4cabe
Pedro <pedrohenriquebandeiramorais@gmail.com>
2022-11-02 20:38:06 -0300

    Carrinho - Cálculo de Frete

--- site.php
+++ site.php
@@ -90,7 +90,8 @@ $app->get("/cart", function() {
 	$page->setTpl("cart", [
 
 		'cart' => $cart->getValues(),
-		'products' => $cart->getProducts()
+		'products' => $cart->getProducts(),
+		'error' => Cart::getMsgError()
 
 	]);
 
@@ -141,6 +142,17 @@ $app->get("/cart/:idproduct/remove" , function($idproduct) {
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /ecommerce/cart");
+	exit;
+
+});
+
+$app->post("/cart/freight", function() {
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /ecommerce/cart");
 	exit;
